ddd1c22244b6ac1ba325fe973acfb7723fcf8935
Samuel <samabera03@gmail.com>
2025-08-05 17:41:52 -0500

    palindrome sloved

---
+++ twosum.rb
@@ -0,0 +1,11 @@
+def twosum (nums, target)
+    hash = {}
+    nums.each_with_index do |num, index|
+        complement = target - num
+        if hash.key?(complement)
+            return [hash[complement], index]
+        end
+        hash[num] = index
+    end
+    []
+end
